c8fc773353d4e14f30970c09e9894078d462f87b
Eric <lowlyherotech@gmail.com>
2019-12-03 17:40:19 -0500

    Constructor and initial state set up in AppJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: '',
+    }
+  }
   render() {
     return (
       <div>
@@ -14,3 +17,6 @@ class App extends React.Component {
 }
 
 export default App;
+
+// All handler functions should live in this file
+// This file will be the container for Todo components
